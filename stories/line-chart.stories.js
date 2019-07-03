51f95066700bedbf53329e9a4a4c8b503695a29f
shenyuanyuan10 <shenyuanyuan@jd.com>
2019-07-03 14:49:35 +0800

    折线图组件

---
+++ stories/line-chart.stories.js
@@ -0,0 +1,65 @@
+import Vue from 'vue'
+import { storiesOf } from '@storybook/vue'
+import singleLineChart from './line-chart'
+import Util from './lib/utils'
+import { fakeResponse2, fakeResponse1 } from './line-chart.fakedata.js'
+
+storiesOf('监控图表(折线图)', module)
+  .add('单数据折线图', () => ({
+    components: { singleLineChart },
+    template: `<div>
+  <single-line-chart 
+    :seriesName="seriesName"
+    :seriesData="seriesData"
+    titleText="内存使用率（%）"
+    v-if="showGraph"
+    titleSubText="纯属虚构"
+    :unit="unit" />
+  </div>`,
+    data() {
+      return {
+        seriesName: [],
+        seriesData: [],
+        unit: '',
+        fakeResponse2,
+        showGraph: false
+      }
+    },
+    created() {
+      this.seriesData = this.fakeResponse2.result.metricDatas[0].data
+      this.seriesData.forEach(element => {
+        element.times = Util.formatDate(element.times)
+      })
+      this.seriesName = this.fakeResponse2.result.metricDatas[0].metric.metricName
+      this.unit = this.fakeResponse2.result.metricDatas[0].metric.calculateUnit
+      this.showGraph = true
+    }
+  }))
+  .add('多数据折线图', () => ({
+    components: { singleLineChart },
+    template: `<div>
+  <single-line-chart 
+    :seriesName="seriesName"
+    :seriesData="seriesData"
+    v-if="showGraph"
+    :unit="unit" />
+  </div>`,
+    data() {
+      return {
+        seriesName: [],
+        seriesData: [],
+        unit: '',
+        fakeResponse1,
+        showGraph: false
+      }
+    },
+    created() {
+      this.seriesData = this.fakeResponse1.result.metricDatas[0].data
+      this.seriesData.forEach(element => {
+        element.times = Util.formatDate(element.times)
+      })
+      this.seriesName = this.fakeResponse1.result.metricDatas[0].metric.metricName
+      this.unit = this.fakeResponse1.result.metricDatas[0].metric.calculateUnit
+      this.showGraph = true
+    }
+  }))
---
+++ stories/line-chart.vue
@@ -0,0 +1,197 @@
+<template>
+  <div id="show-single-chart"
+       :style="style"
+       ref="myEchart">
+  </div>
+</template>
+<script>
+
+import echarts from 'echarts'
+
+export default {
+  props: {
+    // 数值系列的颜色列表
+    color: {
+      type: Array,
+      default () {
+        return ['#66CC00', '#FF9966'];
+      }
+    },
+    // 系列图例名称
+    seriesName: {
+      type: Array,
+      default () {
+        return [];
+      }
+    },
+    // 系列标题名称
+    titleText: {
+      type: String,
+      default: ''
+    },
+    // 系列副标题名称
+    titleSubText: {
+      type: String,
+      default: ''
+    },
+    // 系列提示框单位
+    unit: {
+      type: String,
+      default: '%'
+    },
+    // 纵坐标名称
+    yAxisName: {
+      type: String,
+      default: ''
+    },
+    // 图标宽度
+    width: {
+      type: String,
+      default: '40%'
+    },
+    // 图标高度
+    height: {
+      type: String,
+      default: '300px'
+    },
+    // 系列数据
+    seriesData: {
+      type: Array,
+      default () {
+        return [];
+      }
+    }
+  },
+
+  computed: {
+    // 图标样式
+    style () {
+      return `width:${this.width};height:${this.height}`
+    },
+    // 计算横坐标
+    keys () {
+      return this.seriesData.map(k => k.times)
+    },
+    // 计算系列数据
+    values () {
+      const array = [];
+      this.seriesName.forEach((series, index) => {
+        array.push({
+          data: this.seriesData.map(r => r.value[index]),
+          type: 'line',
+          name: series,
+        })
+      })
+      return array;
+    }
+  },
+
+  mounted () {
+    this.renderChart()
+  },
+
+  methods: {
+    renderChart () {
+      this.chart = echarts.init(this.$refs.myEchart);
+      // 把配置和数据放这里
+      const cfg = {
+        title: {
+          text: this.titleText,
+          subtext: this.titleSubText
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: this.seriesName,
+          bottom: 0,
+          selectedMode: false,
+        },
+        // 内边距
+        grid: {
+          // 有副标题&纵坐标名称y轴下移
+          top: (this.titleSubText || this.yAxisName) ? '28%' : '14%',
+          left: '4%',
+          right: '8%',
+          bottom: '12%',
+          containLabel: true
+        },
+        // 工具栏
+        toolbox: {
+          show: true,
+          feature: {
+            mark: { show: true },
+            magicType: { show: true, type: ['line', 'bar'] },
+            restore: { show: true },
+            saveAsImage: { show: true }
+          }
+        },
+        // 提示框
+        tooltip: {
+          show: true,
+          trigger: 'axis',
+          // 系列提示框显示格式
+          formatter: (paramsArr) => {
+            let cont = `${paramsArr[0].name}<br/>`;
+            cont += paramsArr.map(
+              params => `${params.marker}${params.seriesName}: ${params.value}${this.unit}`
+            ).join('<br/>');
+            return cont;
+          },
+        },
+        // 横坐标配置
+        xAxis: [
+          {
+            type: 'category',
+            axisLine: {
+              onZero: false,
+            },
+            boundaryGap: false,
+            // 横坐标Label 显示
+            axisLabel: {
+              show: true,
+              formatter: (str) => {
+                return str.replace(/^\d{4}-(\d{2}-\d{2}) (\d{2}:\d{2}:\d{2})$/g, '$1' + '\n' + '$2')
+              },
+              interval: 12,
+              showMaxLabel: true
+            },
+            data: this.keys
+          }
+        ],
+        // 纵坐标配置
+        yAxis: [{
+          name: this.yAxisName,
+          type: 'value',
+          // 控制是否显示纵坐标
+          axisLine: {
+            show: false
+          },
+          // 设置y轴分割线
+          splitLine: {
+            show: true,
+            lineStyle: {
+              type: 'dashed'
+            }
+          },
+          // 做坐标分割指定最小最大值
+          min: 0,
+          max: 2
+        }],
+        // 设置系列颜色
+        color: this.color,
+        series: this.values
+      }
+
+      this.chart.setOption(cfg)
+    }
+  }
+}
+</script>
+<style lang="scss" scoped>
+#show-single-chart {
+  border: 1px solid #d9e1e9;
+}
+</style>
+
+
